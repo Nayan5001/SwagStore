6e15150fd1a7071ecb6d3aab0fce3d6da36bcbf6
Nayan Arora <arora.nayan999@gmail.com>
2021-06-14 23:37:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.json()); // support json encoded bodies
