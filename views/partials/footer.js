2b68c38a71d72884cecf97010af5a2c887842929
Nayan Arora <arora.nayan999@gmail.com>
2021-06-15 20:22:09 +0530

    Create footer.js

---
+++ views/partials/footer.js
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>SwagStore</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Goblin+One&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  </head>
+  <body>
+    <div class="footer">
+      <div class="container">
+  <div class="row">
+  <div class="footer-col-1">
+  <h3>Download Our App</h3>
+
+  <p>Download App for Android and ios mobile phone.</p>
+  <div class="app-logo">
+    <a href="workingonit"><img src="images/play-store.png"></a>
+    <a href="workingonit"><img src="images/app-store.png" alt=""></a>
+
+  </div>
+  </div>
+  <div class="footer-col-2">
+  <a href="home"><img src="images/logo1.jpg" alt=""></a>
+  <p>Our purpose Is To Sustainably Make the Pleasure and Benefits of Sports Accessible to the Many.</p>
+  </div>
+  <div class="footer-col-3">
+  <h3>Useful Links</h3>
+  <ul class=footer-ul>
+    <a href="workingonit"><li>Coupons</li></a>
+    <a href="workingonit"><li>Blog Post</li></a>
+    <a href="workingonit"><li>Return Policy</li></a>
+    <a href="workingonit"><li>Join Affiliate</li></a>
+  </ul>
+  </div>
+  <div class="footer-col-4">
+  <h3>Follow Us</h3>
+  <ul class=footer-ul>
+    <a href="workingonit"><li>Facebook</li></a>
+    <a href="workingonit"><li>Twitter</li></a>
+    <a href="workingonit"><li>Instagram</li></a>
+<a href="workingonit"><li>YouTube</li></a>
+  </ul>
+  </div>
+  </div>
+
+      </div>
+      <hr>
+  <p class="copyright">Copyright©2021 @Nayan Arora Ritik Sethi</p>
+    </div>
+  </body>
+</html>
